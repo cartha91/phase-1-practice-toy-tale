94c7ff77bb92169d7bc4a96c70d100138871aec6
steven dulworth <crowscoupetech@proton.me>
2023-10-01 15:50:34 -0500

    toytale

--- src/index.js
+++ src/index.js
@@ -13,3 +13,115 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(toys => {
+      let toyCollection = document.getElementById('toy-collection');
+      toys.forEach(toy => {
+          let card = document.createElement('div');
+          card.className = 'card';
+
+          let name = document.createElement('h2');
+          name.innerText = toy.name;
+          card.appendChild(name);
+
+          let img = document.createElement('img');
+          img.src = toy.image;
+          img.className = 'toy-avatar';
+          card.appendChild(img);
+
+          let likes = document.createElement('p');
+          likes.innerText = `${toy.likes} likes`;
+          card.appendChild(likes);
+
+          let button = document.createElement('button');
+          button.className = 'like-btn';
+          button.id = toy.id;
+          card.appendChild(button);
+
+          toyCollection.appendChild(card);
+      });
+  });
+});
+
+document.querySelector('.add-toy-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  let nameInput = document.querySelector('input[name="name"]');
+  let imageInput = document.querySelector('input[name="image"]');
+
+  let formData = {
+      name: nameInput.value,
+      image: imageInput.value,
+      likes: 0
+  };
+
+  let configObj = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+  };
+
+  fetch('http://localhost:3000/toys', configObj)
+  .then(response => response.json())
+  .then(toy => {
+      let toyCollection = document.getElementById('toy-collection');
+
+      let card = document.createElement('div');
+      card.className = 'card';
+
+      let name = document.createElement('h2');
+      name.innerText = toy.name;
+      card.appendChild(name);
+
+      let img = document.createElement('img');
+      img.src = toy.image;
+      img.className = 'toy-avatar';
+      card.appendChild(img);
+
+      let likes = document.createElement('p');
+      likes.innerText = `${toy.likes} likes`;
+      card.appendChild(likes);
+
+      let button = document.createElement('button');
+      button.className = 'like-btn';
+      button.id = toy.id;
+      card.appendChild(button);
+
+      toyCollection.appendChild(card);
+  })
+  .catch(error => console.error(error));
+});
+
+document.addEventListener('click', function(event) {
+  if (event.target.className === 'like-btn') {
+      let id = event.target.id;
+      let likesElement = event.target.previousSibling;
+      let likes = parseInt(likesElement.innerText.split(' ')[0]) + 1;
+
+      let formData = {
+          likes: likes
+      };
+
+      let configObj = {
+          method: 'PATCH',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          },
+          body: JSON.stringify(formData)
+      };
+
+      fetch(`http://localhost:3000/toys/${id}`, configObj)
+      .then(response => response.json())
+      .then(toy => {
+          likesElement.innerText = `${toy.likes} likes`;
+      })
+      .catch(error => console.error(error));
+  }
+});
